0cd3ad4dcff05ddf55b2af65c7cec71688031b1f
Misha <michaelalex600@gmail.com>
2015-08-13 21:54:05 +0300

    ignore

--- TryMasm 2-0/build.cmd
+++ TryMasm 2-0/build.cmd
@@ -3,8 +3,13 @@
 RC src\resource\rsrc.rc || goto ErrLable
 
 ml /c /coff /Zp4 /D LOG_LEVEL=4 src\main\MasmTry-2.asm || goto ErrLable
-link /subsystem:windows /debug MasmTry-2.obj src\resource\rsrc.res|| goto ErrLable
+link /subsystem:windows /debug MasmTry-2.obj ignore\rsrc.res || goto ErrLable
 
+move MasmTry-2.obj ignore
+move MasmTry-2.ilk ignore
+move MasmTry-2.pdb ignore
+move MasmTry-2.exe ignore
+move src\resource\rsrc.res ignore
 goto end
 :ErrLable
 echo Error
