5ad94e5ac089494e938f088af20fdd8024682ec3
Misha <michaelalex600@gmail.com>
2015-08-20 00:28:43 +0300

    remarks

--- TryMasm 2-0/build.cmd
+++ TryMasm 2-0/build.cmd
@@ -5,7 +5,7 @@ md build
 RC /Fo build\rsrc.res src\resource\rsrc.rc || goto ErrLable
 
 ml /c /coff /Zp4 /D LOG_LEVEL=5 /Fo build\MasmTry-2.obj src\main\MasmTry-2.asm || goto ErrLable
-link /subsystem:windows /debug build\MasmTry-2.obj build\rsrc.res || goto ErrLable
+link /subsystem:windows /debug /OUT:build\MasmTry-2.dbg build\MasmTry-2.obj build\rsrc.res || goto ErrLable
 
 goto end
 :ErrLable
